659bac272d64b70724a717a9c6d768b9fc65c060
EC2 Default User <ec2-user@ip-172-31-12-63.us-east-2.compute.internal>
2021-10-01 16:03:09 +0000

    part 2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -17,7 +17,7 @@ class MoviesController < ApplicationController
         @ratings_to_show = params[:ratings].keys
       end
   
-      @movies = Movie.with_ratings(@ratings_to_show)
+      temp_movie = Movie.with_ratings(@ratings_to_show)
       
       
       hiliteFlag = params[:sort] 
@@ -31,7 +31,7 @@ class MoviesController < ApplicationController
       
       end
       
-      @movies = @movies.order(params[:sort])
+      @movies = temp_movie.order(params[:sort])
       
     end
   
